45a73e7448df709e342e2f1ef1819478ee17d873
malineha <nehamali540@gmail.com>
2021-07-29 14:43:07 +0530

    changes

--- JenkinsGit/src/demo/DemoHello.java
+++ JenkinsGit/src/demo/DemoHello.java
@@ -5,7 +5,7 @@ public class DemoHello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         System.out.print("helloooo");
-        System.out.println("world");
+        
 	}
 
 }
